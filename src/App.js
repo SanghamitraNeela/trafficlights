66437d36a13e2a887233ad4a661d765294837f8b
SanghamitraNeela <97172094+SanghamitraNeela@users.noreply.github.com>
2022-09-30 10:05:18 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./components/styles.css";
+import ChangeColors from "./components/ChangeColors";
+export default function App() {
+  return (
+    <div className="App" style={{ width: "90%", margin: "0 auto" }}>
+      <ChangeColors />
+    </div>
+  );
+}
